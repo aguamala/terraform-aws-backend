3d4075f7eb6a0ed1cf4736f1a7a0e2b960b171f2
deobieta <deobieta@gmail.com>
2017-10-12 17:04:27 -0500

    aws_iam_policy_attachment out of tfstat config module

--- main.tf
+++ main.tf
@@ -14,19 +14,29 @@ module "backend_bucket" {
 module "tfstate_config" {
     source = "./modules/tfstate_config"
 
-    tfstate_write_users  = "${var.tfstate_write_users}"
-    tfstate_write_roles  = "${var.tfstate_write_roles}"
-    tfstate_write_groups = "${var.tfstate_write_groups}"
-
-
     backend_bucket       = "${module.backend_bucket.this_s3_bucket_id}"
     backend_aws_profile  = "${var.backend_aws_profile}"
     backend_aws_region   = "${var.backend_aws_region}"
 
     tfstate_path = "${var.tfstate_path}"
-    workspace    = "${terraform.workspace}"
 }
 
+resource "aws_iam_policy_attachment" "backend_access_global_service" {
+  count = "${terraform.workspace == "default" ? 1 : 0}"
+  name        = "TerraformBackendWriteAccess_${module.backend_bucket.this_s3_bucket_id}_${replace(var.tfstate_path, "/", "_")}"
+  users       = "${var.tfstate_write_users}"
+  roles       = "${var.tfstate_write_roles}"
+  groups      = "${var.tfstate_write_groups}"
+  policy_arn  = "${module.tfstate_config.this_iam_policy_write_access_global_arn}"
+}
 
+resource "aws_iam_policy_attachment" "backend_access_workspace_service" {
+  count = "${terraform.workspace != "default" ? 1 : 0}"
+  name        = "TerraformBackendWriteAccess_${module.backend_bucket.this_s3_bucket_id}_${replace(var.tfstate_path, "/", "_")}_${terraform.workspace}"
+  users       = "${var.tfstate_write_users}"
+  roles       = "${var.tfstate_write_roles}"
+  groups      = "${var.tfstate_write_groups}"
+  policy_arn  = "${module.tfstate_config.this_iam_policy_write_access_workspace_arn}"
+}
 
 
